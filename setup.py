0f16e706eb926753bdf02be5728b5ce2c28b4132
Marco Gario <marco.gario@gmail.com>
2016-10-03 21:53:29 +0200

    Build from SWIG
    The python package contains the .i file and requires SWIG to be
    installed.
    
    * Added simple test
    * Automated Release from CI occur only on Python 3.5

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ from datetime import datetime
 PICOSAT_VERSION='960'
 PICOSAT_DIR='picosat-%s' % PICOSAT_VERSION
 
-PYPICOSAT_MINOR_VERSION='%s' % datetime.utcnow().strftime("%y%m%d")
+#PYPICOSAT_MINOR_VERSION='%s' % datetime.utcnow().strftime("%y%m%d")
+#
+PYPICOSAT_MINOR_VERSION='dev%s' % datetime.utcnow().strftime("%y%m%d%H%M")
 # Major number is Picosat Version, minor number creation date of the bindings
 PYPICOSAT_VERSION='%s.%s' % (PICOSAT_VERSION, PYPICOSAT_MINOR_VERSION)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Build from SWIG definition
 picosat_ext = Extension('_picosat',
                         [os.path.join(BASE_DIR, 'picosat_python.i')],
                         swig_opts=['-I%s' % os.path.join(BASE_DIR, PICOSAT_DIR)],
@@ -19,6 +22,7 @@ picosat_ext = Extension('_picosat',
                         libraries=['picosat'],
                         language='c',
                     )
+
 
 short_description="Picosat SAT-Solver Wrapper"
 long_description=\
@@ -29,10 +33,10 @@ Picosat SAT-Solver Wrapper
 
 pyPicosat provides a basic wrapping around the efficient Picosat SAT-Solver.
 
+To build this wrapper you need SWIG to be installed.
 
 Picosat is developed by Armin Biere, for more information: http://fmv.jku.at/picosat/
 """
-
 
 setup(name='pyPicosat',
       version=PYPICOSAT_VERSION,
